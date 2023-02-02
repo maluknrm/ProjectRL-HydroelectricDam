fe39cc48f98a26bf8daa948410c81a9376ac8f41
maluknrm <90258947+maluknrm@users.noreply.github.com>
2023-02-02 16:35:32 +0100

    run state 2 q learning

--- run_tab_q.py
+++ run_tab_q.py
@@ -12,24 +12,33 @@ from Tab_EpsilonGreedy import *
 from TestEnv import *
 
 
-# parser = argparse.ArgumentParser(description='Arguments for the RL Projects')
-#
-# parser.add_argument('-P', "--path",
-#                     dest="path", type=str,
-#                     help='Testing file path.')
-#
-#
-# args = parser.parse_args()
+parser = argparse.ArgumentParser(description='Arguments for the RL Projects')
+
+parser.add_argument('-P', "--path",
+                    dest="path", type=str,
+                    help='Testing file path.')
 
 
-def main():
+args = parser.parse_args()
+
+
+def run():
+    num_runs = 10
+    n_discrete_actions = 3
+    state_dim = 2
+    state_space = [10, 4]
+    #state_space = [10, 4, 3]
+    num_episodes = 1000
+    reward_shaping_bool = False
+    reward_shaping_type = 2
+
 
     # Data transformation
     data_transformer = DataTransformation()
 
     train_path = "train.xlsx"
-    #val_path = args.path
-    val_path = "validate.xlsx"
+    val_path = args.path
+    #val_path = "validate.xlsx"
 
     data_transformer.transform(dataframe_path=train_path)
     data_transformer.transform(dataframe_path=val_path)
@@ -43,67 +52,14 @@ def main():
     price_thresholds = make_price_threshold(df=train_df)
     water_thresholds = make_water_threshold()
 
-    # Baseline
-    run_baseline(train_df, val_df, price_thresholds, water_thresholds)
-
-
     # Tabular Q-Agent
-    runs = 2
 
-    run_tabQ(runs, train_df, train_path, val_path, val_df, price_thresholds, water_thresholds)
-
-
-
-
-def run_baseline(train_df, val_df, price_thresholds, water_thresholds):
-    print(40 * "-")
-    print("Run baseline")
-
-    n_discrete_actions = 3
-    # state space => water level (index 0), price (index 1)
-    state_space = [10, 4]
-
-    # validate
-    env = DamEnv(n_discrete_actions=n_discrete_actions, state_space=state_space, price_table=val_df, warm_start=False, warm_start_step=2000,
-                shaping=False)
-
-    baseline_policy = BaselinePolicy()
-    basline_agent = BaselineAgent(env=env, policy=baseline_policy,
-                                  num_episodes=1, price_threshold=price_thresholds, water_threshold=water_thresholds)
-
-    episode_lengths, episode_returns, episode_actions, episode_water = basline_agent.execute_quantiles()
-    r_episode_lengths, r_episode_returns, r_episode_actions, r_episode_water = basline_agent.execute_random()
-
-    print("Results:")
-    print(f"The return after applying the qunatiles baseline to the testing dataframe was: {episode_returns[-1][-1]}")
-
-
-
-
-def run_tabQ(runs, train_df, train_path, val_path, val_df, price_thresholds, water_thresholds):
     print(40 * "-")
     print("Run Tabular Q-Learning")
 
-    n_discrete_actions = 3
-    state_dim = 2
-    state_space = [10, 4]
-    #state_space = [10, 4, 3]
-    num_episodes = 1000
-    reward_shaping_bool = True
-    reward_shaping_type = 2
+    for _ in range(num_runs):
 
-
-    dict_run = {
-        "Run": [run_list_item for run_list in [[run] * 2 * num_episodes for run in range(0, runs)] for run_list_item in
-                run_list],
-        "Episode": list(range(0, num_episodes)) * 2 * runs,
-        "Return": [],
-        "Stage": list(["Training"] * num_episodes + ["Validation"] * num_episodes) * runs}
-
-
-    for _ in range(runs):
-
-        # environemnt
+        # environment
         # train
         env_train_vincent = HydroElectric_Test(path_to_test_data=train_path)
         env = DamEnv(n_discrete_actions=n_discrete_actions, state_space=state_space, price_table=train_df, warm_start=False, warm_start_step=2000,
@@ -121,9 +77,6 @@ def run_tabQ(runs, train_df, train_path, val_path, val_df, price_thresholds, wat
 
         Q, avg_rewards, avg_shaped_rewards, episode_lengths, episode_returns, episode_shaped_returns, viz_data, val_returns = agent.execute_qlearning(validation_env=env_val, adaptive_epsilon = True, adapting_learning_rate = False)
 
-        concat_return = list(episode_returns) + [element[0] for element in val_returns]
-        dict_run["Return"] += concat_return
-
 
         # plot for us
         # plt.plot(episode_returns, label='Train Return')
@@ -140,17 +93,14 @@ def run_tabQ(runs, train_df, train_path, val_path, val_df, price_thresholds, wat
         #plt.plot(val_episode_returns)
         #plt.show()
         print("Results:")
-        print(f"The return of the test set after applying the Tabular Q-Learning after {num_episodes} episodes was: {val_episode_returns[-1]}")
+        print(f"The return of test set after applying the Tabular Q-Learning after {num_episodes} episodes was: {val_episode_returns[-1]} (Vincents Env)")
 
         val_episode_lengths, val_episode_returns, val_viz_data = agent.evaluate_policy(env_val, Q_policy)
 
         print("Results:")
-        print(f"The return of the test set after applying the Tabular Q-Learning after {num_episodes} episodes was: {val_episode_returns[-1]}")
+        print(f"The return of the test set after applying the Tabular Q-Learning after {num_episodes} episodes was: {val_episode_returns[-1]} (Our Env)")
         visualise_q(agent, Q, state_dim, reward_shaping_bool, reward_shaping_type)
 
-    #print(dict_run)
-    df = pd.DataFrame(data=dict_run)
-    df.to_csv(f"runs/Statedim_{state_dim}_rewardshaping_{reward_shaping_bool}_type_{reward_shaping_type}")
 
 def visualise_q(agent, Q, state_dim, reward_shaping_bool, reward_shaping_type):
     Q_policy = agent.greedification(Q)
@@ -191,11 +141,4 @@ def visualise_q(agent, Q, state_dim, reward_shaping_bool, reward_shaping_type):
 
 
 
-def run_deep_q(train_df, df_val, price_thresholds, water_thresholds):
-    pass
-
-
-
-
-if __name__ == "__main__":
-    main()
+run()
