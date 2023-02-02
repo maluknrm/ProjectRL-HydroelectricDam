09e3cad6167b3ca8cdf1f040f7b1549965f2de0a
maluknrm <90258947+maluknrm@users.noreply.github.com>
2023-02-02 12:37:26 +0100

    run implementation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 from data_transformation import DataTransformation
 from baselines import *
 from thresholds import *
-from Damn_env import *
+from Dam_env import *
 from TabQAgent import *
 from Tab_EpsilonGreedy import *
 from TestEnv import *
@@ -25,7 +25,8 @@ from TestEnv import *
 
 # parser = argparse.ArgumentParser(description='Arguments for the RL Projects')
 #
-# parser.add_argument('path', metavar='P', type=str,
+# parser.add_argument('-P', "--path",
+#                     dest="path", type=str,
 #                     help='Testing file path.')
 #
 #
@@ -58,12 +59,14 @@ def main():
 
 
     # Tabular Q-Agent
-    run_tabQ(train_df, val_path, val_df, price_thresholds, water_thresholds)
+    runs = 10
+
+    run_tabQ(runs, train_df, train_path, val_path, val_df, price_thresholds, water_thresholds)
 
 
 
 
-def run_baseline(train_df, df_val, price_thresholds, water_thresholds):
+def run_baseline(train_df, val_df, price_thresholds, water_thresholds):
     print(40 * "-")
     print("Run baseline")
 
@@ -72,7 +75,7 @@ def run_baseline(train_df, df_val, price_thresholds, water_thresholds):
     state_space = [10, 4]
 
     # validate
-    env = DamEnv(n_discrete_actions=n_discrete_actions, state_space=state_space, price_table=df_val, warm_start=False, warm_start_step=2000,
+    env = DamEnv(n_discrete_actions=n_discrete_actions, state_space=state_space, price_table=val_df, warm_start=False, warm_start_step=2000,
                 shaping=False)
 
     baseline_policy = BaselinePolicy()
@@ -88,59 +91,78 @@ def run_baseline(train_df, df_val, price_thresholds, water_thresholds):
 
 
 
-def run_tabQ(train_df, train_path, val_path, df_val, price_thresholds, water_thresholds):
+def run_tabQ(runs, train_df, train_path, val_path, val_df, price_thresholds, water_thresholds):
     print(40 * "-")
     print("Run Tabular Q-Learning")
-   
+
     n_discrete_actions = 3
-    state_dim = 3
-    #state_space = [10, 4]
-    state_space = [10, 4, 3]
-    num_episodes = 500
-    reward_shaping_bool = True
+    state_dim = 2
+    state_space = [10, 4]
+    #state_space = [10, 4, 3]
+    num_episodes = 1000
+    reward_shaping_bool = False
     reward_shaping_type = 2
 
-    # environemnt
-    # train
-    env_train_vincent = HydroElectric_Test(path_to_test_data=train_path)
-    env = DamEnv(n_discrete_actions=n_discrete_actions, state_space=state_space, price_table=train_df, warm_start=False, warm_start_step=2000,
-                shaping=reward_shaping_bool, shaping_type=reward_shaping_type)
 
-    # validate with our env
-    env_val = DamEnv(n_discrete_actions=n_discrete_actions, state_space=state_space, price_table=df_val)
-
-    # validate with Vincents env
-    env_val_vincent = HydroElectric_Test(path_to_test_data=val_path)
-
-    agent = QAgent(env=env, policy=TabEpsilonGreedyPolicy(), num_episodes=num_episodes, price_threshold=price_thresholds,
-               water_threshold=water_thresholds, state_dim=state_dim, alpha=0.5)
+    dict_run = {
+        "Run": [run_list_item for run_list in [[run] * 2 * num_episodes for run in range(0, runs)] for run_list_item in
+                run_list],
+        "Episode": list(range(0, num_episodes)) * 2 * runs,
+        "Return": [],
+        "Stage": list(["Training"] * num_episodes + ["Validation"] * num_episodes) * runs}
 
 
-    Q, avg_rewards, avg_shaped_rewards, episode_lengths, episode_returns, episode_shaped_returns, viz_data, val_returns = agent.execute_qlearning(validation_env=env_val, adaptive_epsilon = True, adapting_learning_rate = False)
+    for _ in range(runs):
 
-    # plot for us
-    plt.plot(episode_returns, label='Train Return')
-    plt.plot(val_returns, label='Validation Return')
-    plt.xlabel("Number of Episodes")
-    plt.ylabel("Return")
-    plt.legend()
-    plt.title("Training return with reward shaping")
-    plt.show()
+        # environemnt
+        # train
+        env_train_vincent = HydroElectric_Test(path_to_test_data=train_path)
+        env = DamEnv(n_discrete_actions=n_discrete_actions, state_space=state_space, price_table=train_df, warm_start=False, warm_start_step=2000,
+                    shaping=reward_shaping_bool, shaping_type=reward_shaping_type)
 
-    Q_policy = agent.greedification(Q)
-    val_episode_lengths, val_episode_returns, val_viz_data = agent.evaluate_policy_vincent(env_val_vincent, Q_policy)
+        # validate with our env
+        env_val = DamEnv(n_discrete_actions=n_discrete_actions, state_space=state_space, price_table=val_df)
 
-    plt.plot(val_episode_returns)
-    plt.show()
-    print("Results:")
-    print(f"The return of the test set after applying the Tabular Q-Learning after {num_episodes} episodes was: {val_episode_returns[-1]}")
+        # validate with Vincents env
+        env_val_vincent = HydroElectric_Test(path_to_test_data=val_path)
 
-    val_episode_lengths, val_episode_returns, val_viz_data = agent.evaluate_policy(env_val, Q_policy)
-
-    print("Results:")
-    print(f"The return of the test set after applying the Tabular Q-Learning after {num_episodes} episodes was: {val_episode_returns[-1]}")
+        agent = QAgent(env=env, policy=TabEpsilonGreedyPolicy(), num_episodes=num_episodes, price_threshold=price_thresholds,
+                   water_threshold=water_thresholds, state_dim=state_dim, alpha=0.5)
 
 
+        Q, avg_rewards, avg_shaped_rewards, episode_lengths, episode_returns, episode_shaped_returns, viz_data, val_returns = agent.execute_qlearning(validation_env=env_val, adaptive_epsilon = True, adapting_learning_rate = False)
+
+        concat_return = list(episode_returns) + [element[0] for element in val_returns]
+        dict_run["Return"] += concat_return
+
+
+        # plot for us
+        # plt.plot(episode_returns, label='Train Return')
+        # plt.plot(val_returns, label='Validation Return')
+        # plt.xlabel("Number of Episodes")
+        # plt.ylabel("Return")
+        # plt.legend()
+        # plt.title("Training return with reward shaping")
+        # plt.show()
+
+        Q_policy = agent.greedification(Q)
+        val_episode_lengths, val_episode_returns, val_viz_data = agent.evaluate_policy_vincent(env_val_vincent, Q_policy)
+
+        #plt.plot(val_episode_returns)
+        #plt.show()
+        print("Results:")
+        print(f"The return of the test set after applying the Tabular Q-Learning after {num_episodes} episodes was: {val_episode_returns[-1]}")
+
+        val_episode_lengths, val_episode_returns, val_viz_data = agent.evaluate_policy(env_val, Q_policy)
+
+        print("Results:")
+        print(f"The return of the test set after applying the Tabular Q-Learning after {num_episodes} episodes was: {val_episode_returns[-1]}")
+
+    #print(dict_run)
+    df = pd.DataFrame(data=dict_run)
+    df.to_csv(f"runs/Statedim_{state_dim}_rewardshaping_{reward_shaping_bool}_type_{reward_shaping_type}")
+
+def visualise_q(agent, Q):
 
     if agent.state_dim == 2:
         Q_max_vals = Q.max(axis=2)
@@ -162,9 +184,9 @@ def run_tabQ(train_df, train_path, val_path, df_val, price_thresholds, water_thr
         Q1_max = Q1.max(axis=2)
         Q2_max = Q2.max(axis=2)
 
-        # np.savetxt(f"results/Q_val_table_Q0_space_{agent.state_dim}.csv", Q0_max, delimiter=",")
-        # np.savetxt(f"results/Q_val_table_Q1_space_{agent.state_dim}.csv", Q1_max, delimiter=",")
-        # np.savetxt(f"results/Q_val_table_Q2_space_{agent.state_dim}.csv", Q2_max, delimiter=",")
+        np.savetxt(f"results/Q_val_table_Q0_space_{agent.state_dim}.csv", Q0_max, delimiter=",")
+        np.savetxt(f"results/Q_val_table_Q1_space_{agent.state_dim}.csv", Q1_max, delimiter=",")
+        np.savetxt(f"results/Q_val_table_Q2_space_{agent.state_dim}.csv", Q2_max, delimiter=",")
 
         Q_policy = agent.greedification(Q)
         Q0_eval = np.squeeze(Q[:, :, :1])
@@ -175,9 +197,9 @@ def run_tabQ(train_df, train_path, val_path, df_val, price_thresholds, water_thr
         Q1_pol = Q1_eval.argmax(axis=2)
         Q2_pol = Q2_eval.argmax(axis=2)
 
-        # np.savetxt(f"results/Q_val_policy_Q0_val_space_{agent.state_dim}.csv", Q0_pol, delimiter=",")
-        # np.savetxt(f"results/Q_val_policy_Q1_val_space_{agent.state_dim}.csv", Q1_pol, delimiter=",")
-        # np.savetxt(f"results/Q_val_policy_Q2_val_space_{agent.state_dim}.csv", Q2_pol, delimiter=",")
+        np.savetxt(f"results/Q_val_policy_Q0_val_space_{agent.state_dim}.csv", Q0_pol, delimiter=",")
+        np.savetxt(f"results/Q_val_policy_Q1_val_space_{agent.state_dim}.csv", Q1_pol, delimiter=",")
+        np.savetxt(f"results/Q_val_policy_Q2_val_space_{agent.state_dim}.csv", Q2_pol, delimiter=",")
 
 
 
